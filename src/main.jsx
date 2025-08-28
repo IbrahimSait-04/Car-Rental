fbce444c1c46b5ed62eeb0b6a578420d67d688f3
Ibrahim <saitibrahimsait8369@gmail.com>
2025-08-28 19:21:00 +0530

    updates main file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/Store.js';
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
 <StrictMode>
