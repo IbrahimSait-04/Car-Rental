aa7150bfcb6fdb46957dcfda875fcfe45400a0ef
Ibrahim <saitibrahimsait8369@gmail.com>
2025-08-28 18:59:12 +0530

    carlist moved to assets

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';  
-import { carData } from '../components/cardata';
+import { carData } from '../assets/cardata';
 
 const Booking = () => {
   const [car, setCar] = useState(null);
